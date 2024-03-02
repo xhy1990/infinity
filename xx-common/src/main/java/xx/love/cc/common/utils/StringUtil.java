455b9645b5ede1001b155c4361ac09e01ba9f4ff
xue <598026197@qq.com>
2024-03-02 15:05:22 +0800

    Update StringUtil.java

--- xx-common/src/main/java/xx/love/cc/common/utils/StringUtil.java
+++ xx-common/src/main/java/xx/love/cc/common/utils/StringUtil.java
@@ -6,4 +6,11 @@ package xx.love.cc.common.utils;
  * @Date 2024/3/2
  */
 public class StringUtil {
+
+    public static boolean isEmpty(CharSequence cs) {
+        return cs == null || cs.isEmpty();
+    }
+
+
+
 }
